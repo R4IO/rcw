819b4ba93321b8f4b87e739977b484212a9e8518
bowerth <bo.werth@gmail.com>
2018-06-04 21:45:49 +0200

    only keep 'query' parameter

--- R/create_query_url.R
+++ R/create_query_url.R
@@ -2,31 +2,17 @@
 #'
 #' \code{create_query} creates SDMX query URLs
 #'
-#' @param url SDMX API URL, e.g. \url{http://stats.oecd.org:80/SDMX-JSON/data}
-#' @param flow SDMX data flow, e.g. \code{KEI}
+#' @param data_api_endpoint SDMX API URL, e.g. \url{http://stats.oecd.org:80/SDMX-JSON/data}
 #' @param query SDMX query, e.g.
 #'   \code{"PRINTO01+PRMNTO01.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA.GP.A"}
-#' @param method character string, e.g. \code{all}
-#' @param options character vector, e.g. \code{c("startTime=2015",
-#'   "endTime=2015", "dimensionAtObservation=AllDimensions")}
 #'
 #' @examples
-#' flow <- "KEI"
-#' query <- "PRINTO01+PRMNTO01.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA.GP.A"
-#' opts <- c("startTime=2015", "endTime=2015", "dimensionAtObservation=AllDimensions")
-#' create_query_url(flow = flow, query = query, opts = opts)
-#'
-#' @source \code{jsonlite} was created by Jeroen Ooms et al., see
-#'   \url{https://cran.r-project.org/web/packages/jsonlite/index.html}.
+#' query <- "KEI/PRINTO01+PRMNTO01.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA.GP.A/all?startTime=2015&endTime=2015"
+#' create_query_url(query = query)
 #'
 #' @export
-create_query_url <- function(url="http://stats.oecd.org:80/SDMX-JSON/data",
-                             flow=stop("'flow' must be provided"),
-                             query=stop("'query' must be provided"),
-                             method="all",
-                             opts=c("startTime=2015", "endTime=2015", "dimensionAtObservation=AllDimensions")) {
-
-  options_chr <- paste(opts, collapse = "&")
-  query <- file.path(url, flow, query, paste0(method, "?", options_chr))
+create_query_url <- function(data_api_endpoint"http://stats.oecd.org:80/SDMX-JSON/data",
+                             query=stop("'query' must be provided")) {
+  query <- file.path(url, paste0(query, "&", "dimensionAtObservation=AllDimensions"))
   return(query)
 }
